46f8616e9c19b508dbc034e2e86e588aa848b89b
Umit Sinanoğlu <umit.sinanoglu@formalistech.com>
2023-11-23 15:57:15 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,25 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CarManager carManager = new CarManager(new InMemoryCarDal());
+            foreach (var car in carManager.GetAll())
+            {
+                Console.WriteLine(car.Description);
+            }
+            Console.WriteLine("*******************************************");
+            Console.WriteLine(carManager.GetById(3).Description);
+            Console.WriteLine("*******************************************");
+            foreach (var car in carManager.GetAllByDailyPriceRange(9000, 29000))
+            {
+                Console.WriteLine(car.Id + "-" + car.DailyPrice);
+            }
+        }
+    }
+}
